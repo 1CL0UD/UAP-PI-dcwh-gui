3fc6b54885d9aa8cd93c2f894e85de43d7c5c105
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2022-12-07 19:38:44 +0700

    Add files via upload

---
+++ project/printData_deleteData.py
@@ -0,0 +1,21 @@
+import os
+
+
+def printData():
+    nama_file = input("Masukkan nama file (.txt) :")
+    try:
+        infile = open(nama_file, "r")
+        print(infile.read())
+        infile.close()
+    except FileNotFoundError:
+        print("Nama File", nama_file , "Tidak ada")
+
+
+
+def deleteDaata():
+    nama_file = input("Masukkan nama file (.txt) : ")
+    try:
+        os.remove(nama_file)
+        print("NOTIFIKASI : File", nama_file, "Berhasil Dihapus \n")
+    except FileNotFoundError:
+        print("Nama File", nama_file , "Tidak ditemukan")
