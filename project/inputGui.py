7701b706b1626d67017b943fa5454fa35f19d41b
1CL0UD <89372431+1CL0UD@users.noreply.github.com>
2022-12-07 19:27:43 +0700

    Update inputGui.py

--- project/inputGui.py
+++ project/inputGui.py
@@ -5,14 +5,32 @@ def sendEmbed():
     print('test')
 
 theme('SandyBeach')
+embed = embedClass()
 
 layout = [
-    [Text('Masukkan URL Webhook: '), InputText()],
+    [Text('Masukkan URL Webhook:\t'), InputText()],
+    [Text('Masukkan Content:\t'), InputText()],
+    [Text('Masukkan URL Thumbnail:\t'), InputText()],
+    [Text('Masukkan Author:\t\t'), InputText()],
+    [Text('Masukkan Title:\t\t'), InputText()],
+    [Text('Masukkan Desc:\t\t'), InputText()],
     [Submit(), Cancel()]
 ]
 
 Window = Window('Webhook Discord', layout)
-event, values = Window.read()
+event, val = Window.read()
 Window.close()
 
-print(values[0])
+webhookUrl = val[0]; embed.setWebhookUrl(webhookUrl)
+content = val[1]; embed.setContent(content)
+thumbnailUrl = val[2]; embed.setThumbnailUrl(thumbnailUrl)
+author = val[3]; embed.setAuthorName(author)
+title = val[4]; embed.setTitle(title)
+desc = val[5]; embed.setDesc(desc)
+
+print(webhookUrl)
+print(content)
+print(thumbnailUrl)
+print(author)
+print(title)
+print(desc)
