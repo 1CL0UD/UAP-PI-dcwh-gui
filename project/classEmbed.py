88f060b376765271bc8737b7ce124ee9e3d21605
1CL0UD <89372431+1CL0UD@users.noreply.github.com>
2022-12-10 12:24:11 +0700

    Create classEmbed.py

---
+++ project/classEmbed.py
@@ -0,0 +1,44 @@
+class classEmbed:
+    def getWebhookUrl(self):
+        return self.webhookUrl
+    def setWebhookUrl(self, x):
+        self.webhookUrl = x
+
+    def setContent(self, content):
+        self.content = content
+    def getContent(self):
+        return self.content
+
+    def setThumbnailUrl(self, x):
+        self.thumbnailUrl = x
+    def getThumbnailUrl(self):
+        return self.thumbnailUrl
+
+    def getAuthorName(self):
+        return self.authorName
+    
+    def setAuthorName(self, x):
+        self.authorName = x
+    
+    def setTitle(self, x):
+        self.title = x
+    def getTitle(self):
+        return self.title
+    
+    def getDesc(self):
+        return self.desc
+    
+    def setDesc(self, x):
+        self.desc = x
+
+    def getImgUrl(self):
+        return self.imgUrl
+    
+    def setImgUrl(self, x):
+        self.imgUrl = x
+    
+    def getMention(self):
+        return self.mention
+    
+    def setMention(self, x):
+        self.mention = x
